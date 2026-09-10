ed2ebb8ca044eadb8154b70e2f538fb221b4001c
Udeesha <rathnayakaudeesha@gmail.com>
2026-09-10 13:00:15 +0530

    ci: handle immutable ECR image tags

--- platform/terraform/main.tf
+++ platform/terraform/main.tf
@@ -221,6 +221,7 @@ data "aws_iam_policy_document" "github_actions_ecr" {
       "ecr:CompleteLayerUpload",
       "ecr:InitiateLayerUpload",
       "ecr:PutImage",
+      "ecr:BatchGetImage",
       "ecr:UploadLayerPart"
     ]
 
